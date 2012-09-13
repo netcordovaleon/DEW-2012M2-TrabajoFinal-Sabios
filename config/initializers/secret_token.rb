2d6ee0bc3ddf543619039825c4dba4b9a5fa3f6c
eder sony <ederrafo@gmail.com>
2012-09-13 01:25:48 -0500

    add inicial del proyecto final

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+DewFinal::Application.config.secret_token = '7267a6c5e2d3002cf2e2f0a76e0b69aed2e2918004628baecddf9227f45f2e6309a0a114c3e0fb2ed2cc66b76e88d651b538a45f7fad6c679729f2bb2c9b810a'
