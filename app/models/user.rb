79627091818c518634aa4bf3fc1ff927a01b6517
eder sony <ederrafo@gmail.com>
2012-09-18 23:21:40 -0500

    Corregiendo modificaciones incorrectas no avisadas al equipo

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,3 @@
 class User < ActiveRecord::Base
-  has_many :entrepreneur
-  attr_accesible :addres, :birth_date, :cellular, :document_number, :document_type, :email, :firstname, :lastname, :name, :password, :sexo
-
-
-  
-
+  attr_accessible :addres, :birth_date, :cellular, :document_number, :document_type, :email, :firstname, :lastname, :name, :password, :sexo
 end
--- app/models/wise.rb
+++ app/models/wise.rb
@@ -1,4 +1,4 @@
 class Wise < ActiveRecord::Base
-  belongs_to :user, :class_name => 'User'
-  attr_accessible :apellation, :banck_account, :bank, :summary, :website, :user,:user_id
+  belongs_to :user
+  attr_accessible :apellation, :banck_account, :bank, :summary, :website, :user_id
 end
