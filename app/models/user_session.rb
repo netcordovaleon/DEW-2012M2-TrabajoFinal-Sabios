2b84ba2e6fc147915dba7751071c44ee16e5f574
eder sony <ederrafo@gmail.com>
2012-10-12 19:35:06 -0500

    add archivos de session

---
+++ app/models/user_session.rb
@@ -0,0 +1,12 @@
+class UserSession < Authlogic::Session::Base
+#class UserSession < ActiveRecord::Base
+  def to_key
+     new_record? ? nil : [ self.send(self.class.primary_key) ]
+  end
+  
+  def persisted?
+    false
+  end
+end
+
+
