932589b540cb6f3cdd773cde6d1a9c1150a2d4f7
unknown <alumnos@.upc.edu.pe>
2012-10-13 13:15:58 -0500

    mod finales

--- app/models/sell.rb
+++ app/models/sell.rb
@@ -1,5 +1,5 @@
 class Sell < ActiveRecord::Base
   belongs_to :bussiness_plan
   belongs_to :entrepreneur
-  attr_accessible :address, :code, :credir_car, :date, :bussiness_plan_id, :entrepreneur_id
+  attr_accessible :address, :code, :credir_car, :date, :bussiness_plan_id, :entrepreneur_id, :entrepreneur
 end
